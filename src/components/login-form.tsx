808def6c94b6526f82d44824dce067903fffbed9
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-11-07 09:46:45 +0600

    feat: add loginUser server action and integrate useActionState for form handling

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { useActionState } from "react";
 import { Button } from "./ui/button";
 import { Field, FieldDescription, FieldGroup, FieldLabel } from "./ui/field";
 import { Input } from "./ui/input";
+import { loginUser } from "@/services/auth/loginUser";
 
 const LoginForm = () => {
+  const [state, formAction, isPending] = useActionState(loginUser, null);
+  console.log(state, "state");
+
   return (
-    <form>
+    <form action={formAction}>
       <FieldGroup>
         <div className="grid grid-cols-1 gap-4">
           {/* Email */}
@@ -35,9 +40,8 @@ const LoginForm = () => {
         </div>
         <FieldGroup className="mt-4">
           <Field>
-            <Button type="submit">
-              {/* {isPending ? "Logging in..." : "Login"} */}
-              Login
+            <Button type="submit" disabled={isPending}>
+              {isPending ? "Logging in..." : "Login"}
             </Button>
 
             <FieldDescription className="px-6 text-center">
