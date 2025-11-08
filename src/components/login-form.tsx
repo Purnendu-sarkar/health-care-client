54b08cbf078f4386c85878c0584693d16fbe0a47
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-11-09 01:03:01 +0600

    feat(proxy.ts): implement JWT-based route protection and role-based access control middleware

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -7,7 +7,7 @@ import { Field, FieldDescription, FieldGroup, FieldLabel } from "./ui/field";
 import { Input } from "./ui/input";
 import { loginUser } from "@/services/auth/loginUser";
 
-const LoginForm = () => {
+const LoginForm = ({ redirect }: { redirect?: string }) => {
   const [state, formAction, isPending] = useActionState(loginUser, null);
   console.log(state, "state");
 
@@ -22,6 +22,7 @@ const LoginForm = () => {
 
   return (
     <form action={formAction}>
+      {redirect && <input type="hidden" name="redirect" value={redirect} />}
       <FieldGroup>
         <div className="grid grid-cols-1 gap-4">
           {/* Email */}
