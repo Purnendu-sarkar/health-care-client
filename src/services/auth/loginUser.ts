2387d7e9306f38b18341ed21b309f023fe8327d0
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-11-11 21:58:18 +0600

    update(login): role based redirect add + token cookie setup fix

--- src/services/auth/loginUser.ts
+++ src/services/auth/loginUser.ts
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use server"
 
+import { getDefaultDashboardRoute, isValidRedirectForRole, UserRole } from "@/lib/auth-utils";
 import { parse } from "cookie";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import z from "zod";
 
 const loginValidationZodSchema = z.object({
@@ -18,10 +21,9 @@ const loginValidationZodSchema = z.object({
 
 export const loginUser = async (_currentState: any, formData: any): Promise<any> => {
     try {
+        const redirectTo = formData.get('redirect') || null;
         let accessTokenObject: null | any = null;
         let refreshTokenObject: null | any = null;
-
-
         const loginData = {
             email: formData.get('email'),
             password: formData.get('password'),
@@ -47,9 +49,7 @@ export const loginUser = async (_currentState: any, formData: any): Promise<any>
             headers: {
                 "Content-Type": "application/json",
             },
-        })
-
-        const result = await res.json();
+        });
 
         const setCookieHeaders = res.headers.getSetCookie();
         console.log(setCookieHeaders, "setCookie")
@@ -94,12 +94,30 @@ export const loginUser = async (_currentState: any, formData: any): Promise<any>
             path: refreshTokenObject.Path || "/",
             sameSite: refreshTokenObject['SameSite'] || "none",
         });
-        return result;
+        const verifiedToken: JwtPayload | string = jwt.verify(accessTokenObject.accessToken, process.env.JWT_ACCESS_SECRET as string);
+
+        if (typeof verifiedToken === "string") {
+            throw new Error("Invalid token");
+
+        }
+
+        const userRole: UserRole = verifiedToken.role;
 
 
-        return res;
+        if (redirectTo) {
+            const requestedPath = redirectTo.toString();
+            if (isValidRedirectForRole(requestedPath, userRole)) {
+                redirect(requestedPath);
+            } else {
+                redirect(getDefaultDashboardRoute(userRole));
+            }
+        }
 
-    } catch (error) {
+    } catch (error: any) {
+        // Re-throw NEXT_REDIRECT errors so Next.js can handle them
+        if (error?.digest?.startsWith('NEXT_REDIRECT')) {
+            throw error;
+        }
         console.log(error);
         return { error: "Login failed" };
     }
