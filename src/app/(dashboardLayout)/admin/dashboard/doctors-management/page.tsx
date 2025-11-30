a95319b4c331dcb4f621fe03b5b2b5fe57f3d93f
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-11-30 10:29:24 +0600

    Adding Pagination Component For Doctors Table

--- src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
@@ -3,15 +3,26 @@ import DoctorsTable from "@/components/modules/Admin/DoctorsManagement/DoctorsTa
 import RefreshButton from "@/components/shared/RefreshButton";
 import SearchFilter from "@/components/shared/SearchFilter";
 import SelectFilter from "@/components/shared/SelectFilter";
+import TablePagination from "@/components/shared/TablePagination";
 import { TableSkeleton } from "@/components/shared/TableSkeleton";
+import { queryStringFormatter } from "@/lib/formatters";
 import { getDoctors } from "@/services/admin/doctorManagement";
 import { getSpecialities } from "@/services/admin/specialitiesManagement";
 import { ISpecialty } from "@/types/specialities.interface";
 import { Suspense } from "react";
 
-const dminDoctorsManagementPage = async () => {
+const AdminDoctorsManagementPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) => {
+  const searchParamsObj = await searchParams;
+  const queryString = queryStringFormatter(searchParamsObj); // {searchTerm: "John", speciality: "Cardiology" => "?searchTerm=John&speciality=Cardiology"}
   const specialitiesResult = await getSpecialities();
-  const doctorsResult = await getDoctors();
+  const doctorsResult = await getDoctors(queryString);
+  const totalPages = Math.ceil(
+    doctorsResult.meta.total / doctorsResult.meta.limit
+  );
   return (
     <div className="space-y-6">
       <DoctorsManagementHeader specialities={specialitiesResult.data} />
@@ -28,10 +39,17 @@ const dminDoctorsManagementPage = async () => {
         <RefreshButton />
       </div>
       <Suspense fallback={<TableSkeleton columns={10} rows={10} />}>
-        <DoctorsTable doctors={doctorsResult.data} />
+        <DoctorsTable
+          doctors={doctorsResult.data}
+          // specialities={specialitiesResult.data}
+        />
+        <TablePagination
+          currentPage={doctorsResult.meta.page}
+          totalPages={totalPages}
+        />
       </Suspense>
     </div>
   );
 };
 
-export default dminDoctorsManagementPage;
+export default AdminDoctorsManagementPage;
